02175d8b27b7245e84e505cf6dd637181859b225
Jon Udell <judell@turbot.com>
2022-09-03 14:50:09 -0700

    add tf

---
+++ ss.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    steampipecloud = {
+      source = "turbot/steampipecloud"
+    }
+  }
+}
+
+resource "steampipecloud_workspace_mod" "steampipe_stats" {
+  organization = "acme" 
+  workspace_handle = "jon"
+  path = "github.com/judell/steampipe-stats"
+  constraint = "v0.12"
+}
