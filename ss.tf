7a0a1b3ca108a238e02c63a0b27272af532caacb
judell <jon@jonudell.info>
2022-09-07 08:27:09 -0700

    bump constraint

--- ss.tf
+++ ss.tf
@@ -10,5 +10,5 @@ resource "steampipecloud_workspace_mod" "steampipe_stats" {
   organization = "acme" 
   workspace_handle = "jon"
   path = "github.com/judell/steampipe-stats"
-  constraint = "v0.16"
+  constraint = "v0.18"
 }
